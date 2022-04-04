582e614a064521dc9c72f6b4238312155c37de36
nkirti28 <nalawade.kirti@gmail.com>
2022-04-03 21:46:18 -0500

    log added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
 window.alert("welcome to game");
+
+// create function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+//call the function
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+var myName = "Kirti";
+window.alert(playerName);
+console.log(playerName);
+
+console.log(myName);
