08bdc542edcd0d0f7725331b9fb20e03791cc5b8
nkirti28 <nalawade.kirti@gmail.com>
2022-04-03 22:11:44 -0500

    log added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,57 @@
 window.alert("welcome to game");
 
-// create function named "fight"
-
-function fight() {
-  window.alert("The fight has begun!");
-}
-
-//call the function
-fight();
-
 var playerName = window.prompt("What is your robot's name?");
 var myName = "Kirti";
-window.alert(playerName);
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+//you can log maultiple variables on same time
+console.log(playerName, playerHealth, playerAttack);
 
-console.log(myName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// create function named 'fight'
+
+function fight() {
+  window.alert("Welcome to Robot-Gladiators!");
+  //update enemyHealth
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  // update playerHealth
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+}
+
+//execute/ call the function
+fight();
